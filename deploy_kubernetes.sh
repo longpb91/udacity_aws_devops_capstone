499917a191a8788d1963505c36fabd000cf6fdd8
LongPB <longpb.91@gmail.com>
2022-12-29 02:30:54 +0700

    build k8s

--- deploy_kubernetes.sh
+++ deploy_kubernetes.sh
@@ -3,13 +3,13 @@
 spawn ssh -i "capstone-keypair.pem" ec2-user@ec2-44-201-115-166.compute-1.amazonaws.com
 expect "Are you sure" { send "yes\r" } 
 expect "ec2-user" { send "kubectl get nodes\r" }
-expect "ec2-user" { send "kubectl run capstone_aws_devops --image=huylq1999/capstone_aws_devops --port=5000\r" }
+expect "ec2-user" { send "kubectl run capstone_aws_devops_pj --image=longpb/capstone_aws_devops_pj --port=5000\r" }
 expect "ec2-user" { send "kubectl get pods\r" }
-expect "ec2-user" { send "kubectl port-forward capstone_aws_devops 5000:5000\r" }
+expect "ec2-user" { send "kubectl port-forward capstone_aws_devops_pj 5000:5000\r" }
 expect "ec2-user" { send "kubectl get deployments\r" }
-expect "ec2-user" { send "kubectl set image deployment/capstone_aws_devops capstone_aws_devops=capstone_aws_devops:latest\r" }
-expect "ec2-user" { send "kubectl rollout status deployment/capstone_aws_devops\r" }
-expect "ec2-user" { send "kubectl rollout undo deployment/capstone_aws_devops\r" }
+expect "ec2-user" { send "kubectl set image deployment/capstone_aws_devops_pj capstone_aws_devops_pj=capstone_aws_devops_pj:latest\r" }
+expect "ec2-user" { send "kubectl rollout status deployment/capstone_aws_devops_pj\r" }
+expect "ec2-user" { send "kubectl rollout undo deployment/capstone_aws_devops_pj\r" }
 expect "ec2-user" { send "eksctl delete cluster -f eks_cluster.yml\r" }
 expect "ec2-user" { send "logout\r" } 
 interact
