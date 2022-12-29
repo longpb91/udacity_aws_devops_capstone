1233d1fb00f8201bbf26fc196b3994cbbeb04c0c
LongPB <longpb.91@gmail.com>
2022-12-29 23:50:15 +0700

    build image in ecr

--- deploy_k8s.sh
+++ deploy_k8s.sh
@@ -5,9 +5,9 @@ aws eks update-kubeconfig --region us-east-1 --name longpb-cluster
 
 export KUBECONFIG=~/.kube/config
 # Set variable to config file
-# cat flask_deployment.yaml | envsubst '${CIRCLE_SHA1}' > flask_config_deployment.yaml
+cat flask_deployment.yml | envsubst '${CIRCLE_SHA1}' > flask_config_deployment.yml
 # Deploy
-kubectl apply -f flask_deployment.yml
+kubectl apply -f flask_config_deployment.yml
 
 # this wait for 'Available=True' only works for initial deployment, not rolling
 kubectl wait deployment myflaskapp-deployment --for condition=Available=True --timeout=90s
